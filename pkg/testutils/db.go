466b26d41a4b2abf986e57a18546aec18f6d3863
Alexandre Vinyals Valdepeñas <alevinval@gmail.com>
2020-11-03 10:18:23 +0100

    tests: fix concurrency issues

--- pkg/testutils/db.go
+++ pkg/testutils/db.go
@@ -1,12 +1,15 @@
 package testutils
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path"
 
 	"github.com/dgraph-io/badger/v2"
 )
+
+var dbCounter int
 
 // WithDB provides a badger DB instance to run tests against
 func WithDB(fn func(db *badger.DB)) {
@@ -21,13 +24,12 @@ func WithDB(fn func(db *badger.DB)) {
 
 func openBadgerDB() (*badger.DB, error) {
 	tmpPath := os.TempDir()
-	storePath := path.Join(tmpPath, "test-badger.db")
+	storePath := path.Join(tmpPath, fmt.Sprintf("test-badger-%d.db", dbCounter))
+	dbCounter += 1
 
 	os.RemoveAll(storePath)
 
-	opts := badger.DefaultOptions("")
-	opts.Dir = storePath
-	opts.ValueDir = storePath
+	opts := badger.DefaultOptions(storePath)
 	opts.Logger = nil
 
 	log.Printf("opening badger db in %s", storePath)
