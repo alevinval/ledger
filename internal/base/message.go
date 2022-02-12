94798335cb253fe7066222e26d3079d8093317b4
Alexandre Vinyals Valdepeñas <alevinval@gmail.com>
2022-02-12 09:49:12 +0100

    internal/base: clean unused interface, clarify docs

--- internal/base/message.go
+++ internal/base/message.go
@@ -1,17 +1,19 @@
 package base
 
-type Commitable interface {
-	Commit(uint64) error
-}
-
 type (
-	// Message structure used to represent read results
+	// Message interface used to pass read results from a ledger.Reader
+	// Provides the data payload and the offset in the log
 	Message interface {
 		Offset() uint64
 		Data() []byte
 	}
 
-	// PartitionedMessage structure that represents messages from a partitioned reader
+	// PartitionedMessage interface used to pass read results from a ledger.PartitionedReader
+	// Provides the data payload and the offset in the log.
+	//
+	// Additionally, provides an interface to commit the reads, when using a PartitionedReader
+	// the partition from which the message is read is hidden from the user, this simplifies
+	// the usage of the library.
 	PartitionedMessage interface {
 		Commit() error
 		Offset() uint64
